d0c3116bd87776414d3995773c8bc5dbd5308ad6
Josh Long <josh@joshlong.com>
2018-10-06 02:40:49 -0700

    bootstrap

---
+++ src/main/java/com/example/bootstrap/BootstrapApplication.java
@@ -0,0 +1,12 @@
+package com.example.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapApplication.class, args);
+	}
+}
