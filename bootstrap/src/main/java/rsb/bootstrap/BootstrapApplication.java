071b8b4fb3b7d87267b2f5bdb6dc16149bbd654a
Josh Long <josh@joshlong.com>
2018-10-07 01:28:41 -0700

    up

--- bootstrap/src/main/java/rsb/bootstrap/BootstrapApplication.java
+++ bootstrap/src/main/java/rsb/bootstrap/BootstrapApplication.java
@@ -1,4 +1,4 @@
-package com.example.bootstrap;
+package rsb.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- bootstrap/src/test/java/rsb/bootstrap/BootstrapApplicationTests.java
+++ bootstrap/src/test/java/rsb/bootstrap/BootstrapApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.bootstrap;
+package rsb.bootstrap;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
