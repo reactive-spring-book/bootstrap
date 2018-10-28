7e09d2c3bf19b7a822a0dd3162e39486fe99a49e
Josh Long <josh@joshlong.com>
2018-10-28 23:51:48 +0100

    commiting any unsaved work @ Sun Oct 28 23:51:47 CET 2018.

--- src/main/java/rsb/bootstrap/enable/Application.java
+++ src/main/java/rsb/bootstrap/enable/Application.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.support.TransactionTemplate;
 import rsb.bootstrap.CustomerService;
 import rsb.bootstrap.DataSourceConfiguration;
 import rsb.bootstrap.Demo;
 import rsb.bootstrap.SpringUtils;
+import rsb.bootstrap.templates.TransactionTemplateCustomerService;
 
 import javax.sql.DataSource;
 
@@ -22,8 +24,13 @@ import javax.sql.DataSource;
 public class Application {
 
 	@Bean
-	PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
-		return new DataSourceTransactionManager(dataSource);
+	PlatformTransactionManager transactionManager(DataSource ds) {
+		return new DataSourceTransactionManager(ds);
+	}
+
+	@Bean
+	TransactionTemplate transactionTemplate(PlatformTransactionManager tm) {
+		return new TransactionTemplate(tm);
 	}
 
 	public static void main(String args[]) {
--- src/main/java/rsb/bootstrap/enable/TransactionalCustomerService.java
+++ src/main/java/rsb/bootstrap/enable/TransactionalCustomerService.java
@@ -9,25 +9,23 @@ import javax.sql.DataSource;
 import java.util.Collection;
 
 @Service
+@Transactional
 class TransactionalCustomerService extends BaseCustomerService {
 
 	TransactionalCustomerService(DataSource dataSource) {
 		super(dataSource);
 	}
 
-	@Transactional
 	@Override
 	public Collection<Customer> save(String... names) {
 		return super.save(names);
 	}
 
 	@Override
-	@Transactional
 	public Customer findById(Long id) {
 		return super.findById(id);
 	}
 
-	@Transactional
 	@Override
 	public Collection<Customer> findAll() {
 		return super.findAll();
